6788ed32b4d6be0f17ee44292aa25c8852ee0bc0
Chi-Feng Tsai <ktpss95112@yahoo.com.tw>
2018-12-03 10:39:42 +0800

    finish task 5

--- HW3/functions.py
+++ HW3/functions.py
@@ -1,7 +1,7 @@
 from vpython import *
 import matplotlib.pyplot as plt
 
-def Homework(position, velocity):
+def Homework(position, velocity, figname, draw_q=False):
     class SimpleHarmonicOscillation():
         dt = 0.0001
         t = 0
@@ -11,22 +11,19 @@ def Homework(position, velocity):
         ground = box(pos=vec(0, -0.6, 0), length=10, width=3, height=0.1)
         SHOW_ANIMATION = False
 
-
         def __init__(self, position, velocity):
             self.init_pos = position
             self.init_vel = velocity
             self.ball1 = sphere(dx=position[0], pos=vec(-self.spring_length+position[0],0,0), velocity=vec(velocity[0],0,0), radius=0.25)
             self.ball2 = sphere(dx=position[1], pos=vec(                    position[1],0,0), velocity=vec(velocity[1],0,0), radius=0.25)
             self.ball3 = sphere(dx=position[2], pos=vec( self.spring_length+position[2],0,0), velocity=vec(velocity[2],0,0), radius=0.25)
-            self.fig, self.ax = plt.subplots()
-            self.ax.set_title(f'x-t graph (x1,x2,x3)=(%.2f,%.2f,%.2f) (v1,v2,v3)=(%.2f,%.2f,%.2f)'%(*position, *velocity))
-            self.ax.set_xlabel('t (sec)')
-            self.ax.set_ylabel('x (m)')
-            self.ax.axhline(y=0, color='k', linewidth=0.5)
-            self.list_x1 = [self.ball1.pos.x]
-            self.list_x2 = [self.ball2.pos.x]
-            self.list_x3 = [self.ball3.pos.x]
+            self.list_x1 = [self.ball1.dx]
+            self.list_x2 = [self.ball2.dx]
+            self.list_x3 = [self.ball3.dx]
             self.list_t = [self.t]
+            self.list_q1 = [self.ball1.dx + 2*self.ball2.dx + self.ball3.dx]
+            self.list_q2 = [self.ball1.dx - self.ball3.dx]
+            self.list_q3 = [self.ball1.dx - 2*self.ball2.dx + self.ball3.dx]
 
         def iterate(self):
             a1 = self.k*(self.ball2.dx - self.ball1.dx)
@@ -52,20 +49,24 @@ def Homework(position, velocity):
                 and abs(self.ball1.velocity.x-self.init_vel[0])<self.eps\
                 and abs(self.ball2.velocity.x-self.init_vel[1])<self.eps\
                 and abs(self.ball3.velocity.x-self.init_vel[2])<self.eps:
-                print(f'Period: {self.t}')
+                print(f'Period: {self.t:.3f}')
                 self.period = self.t
 
         def run(self):
             # debug
             #counter = 0
-            while self.t <= 5:
+            while self.t <= 6:
                 if self.SHOW_ANIMATION: rate(1/self.dt)
                 self.iterate()
 
-                self.list_x1.append(self.ball1.pos.x)
-                self.list_x2.append(self.ball2.pos.x)
-                self.list_x3.append(self.ball3.pos.x)
+                self.list_x1.append(self.ball1.dx)
+                self.list_x2.append(self.ball2.dx)
+                self.list_x3.append(self.ball3.dx)
                 self.list_t.append(self.t)
+                self.list_q1.append(self.ball1.dx + 2*self.ball2.dx + self.ball3.dx)
+                self.list_q2.append(self.ball1.dx - self.ball3.dx)
+                self.list_q3.append(self.ball1.dx - 2*self.ball2.dx + self.ball3.dx)
+
 
                 # debug
                 '''if counter%10 == 0:
@@ -81,12 +82,27 @@ def Homework(position, velocity):
                     self.print_peroid()
 
         def draw(self):
-            self.ax.plot(self.list_t, self.list_x1, label='x1')
-            self.ax.plot(self.list_t, self.list_x2, label='x2')
-            self.ax.plot(self.list_t, self.list_x3, label='x3')
+            self.fig, self.ax = plt.subplots()
 
-            self.ax.legend()
-            #plt.subplots_adjust()
+            if not draw_q:
+                self.ax.set_title(f'x-t graph (x1,x2,x3)=(%.2f,%.2f,%.2f) (v1,v2,v3)=(%.2f,%.2f,%.2f)'%(*position, *velocity))
+                self.ax.set_xlabel('t (sec)')
+                self.ax.set_ylabel('x (m)')
+                self.ax.plot(self.list_t, self.list_x1, label='x1')
+                self.ax.plot(self.list_t, self.list_x2, label='x2')
+                self.ax.plot(self.list_t, self.list_x3, label='x3')
+                self.ax.legend(loc='upper right')
+                self.ax.grid(b=True)
+            else:
+                self.ax.set_title(f'q-t graph')
+                self.ax.set_xlabel('t (sec)')
+                self.ax.set_ylabel('q (m)')
+                self.ax.plot(self.list_t, self.list_q1, label='q1')
+                self.ax.plot(self.list_t, self.list_q2, label='q2')
+                self.ax.plot(self.list_t, self.list_q3, label='q3')
+                self.ax.legend(loc='upper right')
+                self.ax.grid(b=True)
+            self.fig.savefig(figname)
 
         def __del__(self):
             self.ball1.visible = False
--- HW3/main.py
+++ HW3/main.py
@@ -1,13 +1,14 @@
 from functions import Homework, plt
 
 # task 3
-Homework((0.1, 0, -0.1), (0, 0, 0))
+Homework((0.1, 0, -0.1), (0, 0, 0), 'task_3.png')
 
 # task 4
-Homework((0, 0, 0), (0.2, -0.2, 0.2))
+Homework((0, 0, 0), (0.2, -0.2, 0.2), 'task_4.png')
 
 # task 5
-#Homework((), ())
+Homework((0.4, -0.1, -0.3), (0.5, -0.4, 0.2), 'task_5_x.png')
+Homework((0.4, -0.1, -0.3), (0.5, -0.4, 0.2), 'task_5_q.png', draw_q=True)
 
-plt.xticks(range(0, 6, 1))
+plt.xticks(range(0, 7))
 plt.show()
