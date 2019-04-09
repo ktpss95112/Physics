1fb275038fb247840e1fef478bfd186f53afc828
Chi-Feng Tsai <ktpss95112@yahoo.com.tw>
2019-04-09 20:21:06 +0800

    substitute a magic number with a variable

--- HW5/main.py
+++ HW5/main.py
@@ -7,17 +7,18 @@ v1 = 3
 v2 = 0
 v3 = 2
 v4 = -2
+scale = 5
 
 scene1 = canvas( width=600, height=600, align='left' )
 
 sphlist = [ [ sphere( pos=vec(x, y, 0) , radius=0.5 ) for x in range(-maxx, maxx+1) ] for y in range(-maxy, maxy+1) ]
 
 for i in range(-maxx, maxx+1):
-    sphlist[0][i].pos.z = v1 * 5
-    sphlist[-1][i].pos.z = v3 * 5
+    sphlist[0][i].pos.z = v1 * scale
+    sphlist[-1][i].pos.z = v3 * scale
 for i in range(-maxy, maxy+1):
-    sphlist[i][0].pos.z = v2 * 5
-    sphlist[i][-1].pos.z = v4 * 5
+    sphlist[i][0].pos.z = v2 * scale
+    sphlist[i][-1].pos.z = v4 * scale
 sphlist[0][0].opacity = sphlist[0][-1].opacity = sphlist[-1][0].opacity = sphlist[-1][-1].opacity = 0
 
 for _ in range(1000):
